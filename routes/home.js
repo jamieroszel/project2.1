44b17b3eb474b0342fd6241387ec858468b97d01
jamieroszel <jamie.s.roszel@gmail.com>
2021-04-14 09:51:43 -0400

    add index, 404 views. add head, header, footer partials

--- routes/home.js
+++ routes/home.js
@@ -10,9 +10,24 @@ const router = require("express").Router()
 ///////////////////////////////
 // Router Routes
 ////////////////////////////////
+
+// Index Router
 router.get("/", (req, res) => {
     res.render("home")
 })
+
+// About Router
+router.get("/about", (req, res) => {
+    res.render("about")
+})
+
+// 404 Router
+router.use((req, res) => {
+    res.status(404).render('404')
+})
+
+
+
 
 ///////////////////////////////
 // Export Router
